b4fa640dbfb6e24ad57c2966ab643e5c9cece699
Brendan Kenna <brendan.kenna.bk@gmail.com>
2023-05-09 12:11:03 +0100

    model factory & started data store-model bridge

--- code/audioHandler/AudioHandler.py
+++ code/audioHandler/AudioHandler.py
@@ -4,3 +4,94 @@ Created on Mon May 26 17:56 2023
 
 @author: kenna
 """
+
+from model import Factory
+import librosa
+import numpy as np
+import zlib
+import soundfile as sf
+import os
+import boto3
+
+class AudioHandler:
+    """
+    Class to bridge datastore to audio data. Liken to utility
+    """
+
+    def __init__(self):
+        """
+        Initialize with audio factory
+        """
+        self.modelFactory = Factory.AudioFactory()
+        self.s3Client = s3Client = boto3.client('s3')
+
+
+    def loadAudio(self, trackPath):
+        """
+        Load audio into numpy array
+        """
+
+        # Read audio
+        wave, sampleRate = librosa.load(trackPath)
+        frameCount = wave.shape[0]
+        duration = round(float(frameCount / sampleRate), 2)
+
+        # Construct audio models
+        output = {
+            "audioSignal": self.modelFactory.makeAudioSignal(self.compressAudioSignal),
+            "audioMetaData": self.modelFactory.makeAudioMeta(sampleRate, frameCount, duration)
+        }
+        return output
+
+
+    def compressAudioSignal(self, signal: bytes):
+        """
+        Compress input audio signal
+        """
+        return zlib.compress(signal)
+
+
+    def deCompressAudioSignal(self, signal: bytes):
+        """
+        Decompress input audio signal
+        """
+        return zlib.decompress(signal)
+
+
+    def rebuildAudio(self, comprSignal: bytes)
+        """
+        Rebuild audio signal from compressed signal
+        """
+        return np.frombuffer(comprSignal, dtype="float32")
+
+
+    def writeAudio(self, audioSignal: bytes, outPath: str):
+        """
+        Write audio signal to file
+        """
+        if not os.path.exists(outPath):
+            sf.write(
+                outPath,
+                self.audioSignal,
+                self.sampleRate
+            )
+            print(f'Audio signal written to {outPath}')
+        else:
+            print(f'Error file {outPath} already exists.')
+
+
+    def fetchAudio(self, bucket: str, key: str, outPath: str):
+
+        # Initialize vars
+        fileName = os.path.basename(key)
+        trackName = fileName.replace(".wav", "")
+        outPath = str(os.getcwd() + "/tmp/")
+        trackPath = str(outPath + fileName)
+
+        # Fetch track
+        os.makedirs(os.path.dirname(outPath), exist_ok = True)
+        self.s3Client.download_file(bucket, key, str(outPath + fileName))
+        return {
+            'trackName': trackName,
+            'trackPath': trackPath
+        }
---
+++ code/model/Factory.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tues May 09 11:11 2023
+
+@author: kenna
+"""
+
+from audioHandler import AudioHandler
+import AudioSignal as audS
+import AudioMetaData as audMD
+import TrackMetaData as tMD
+import AudioModel as audMod
+from time import time
+
+class AudioFactory:
+    """
+    Class responsible for instantiating model objects
+    """
+
+    def __init__(self):
+        """
+        Initialize with object counters
+        """
+        self.audioHandler = AudioHandler()
+        self.audioMetaCount = 0
+        self.trackMetaCount = 0
+        self.audioSignalCount = 0
+        self.audioModelCount = 0
+
+
+    # Could be private
+    def makeAudioSignal(self, comprSignal: bytes):
+        """
+        Instantiate an AudioSignal object from compressed audio byte array
+        """
+        self.audioSignalCount += 1
+        return audS.AudioSignal(comprSignal)
+
+
+    # Could be private
+    def makeAudioMeta(self, sampleRate: int, frameCount: int, duration: float):
+        """
+        Instantiate an AudioMetaData object
+        """
+        self.audioMetaCount += 1
+        return audMD.AudioMetaData(sampleRate, frameCount, duration)
+
+
+    # Could be private
+    def makeTrackMeta(self,trackName: str, owner: str, nowTimeStamp: time):
+        """
+        Instantiate a TrackMetaData object
+        """
+        self.trackMetaCount += 1
+        return tMD.TrackMetaData(trackName, owner, nowTimeStamp)
+
+
+    # Could be private
+    def makeAudioModel(self, audioSignal: audS.AudioSignal, audioMeta: audMD.AudioMetaData, trackMeta: tMD.TrackMetaData):
+        """
+        Instantiate an AudioModel object
+        """
+        self.audioModelCount += 1
+        return audMod.AudioModel(audioSignal, audioMeta, trackMeta)
+
+
+    # Should be public
+    # AudioHandler can read audio, know numpy, and provide bytes
+    def constructDataStoreModel(
+            self, trackName: str, owner: str, nowTimeStamp: time,
+            waveBytes: bytes, sampleRate: int, frameCount: int,
+            duration: float
+        ):
+        """
+        Construct an audio data store model from input
+        """
+        audioSignal = self.makeAudioSignal(waveBytes)
+        audioMeta = self.makeAudioMeta(sampleRate, frameCount, duration)
+        trackMeta = self.makeTrackMeta(trackName, owner, nowTimeStamp)
+        return self.makeAudioModel(audioSignal, audioMeta, trackMeta)
